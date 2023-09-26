5fef5383a8f224363aeeccdd428ac9f488347845
Agung Sundoro <muhagung2001@gmail.com>
2023-09-26 20:32:12 +0700

    feat: initial commit add directories and files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kami Kerja Remote WordPress Plugin
+
+Plugin ini dibuat untuk membantu mengembangkan fungsionalitas website KKR.
---
+++ kkr.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Kami Kerja Remote
+ * Plugin URI:        https://github.com/kamikerjaremote/kkr
+ * Description:       Kami Kerja Remote WordPress Plugin
+ * Version:           0.0.1
+ * Author:            Agung Sundoro
+ * Author URI:        https://agung2001.github.io
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * SOFTWARE LICENSE INFORMATION
+ *
+ * For detailed information regards to the licensing of
+ * this software, please review the license.txt
+*/
